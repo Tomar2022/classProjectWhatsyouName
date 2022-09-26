a2cf7a73670c21d9024d4165ad94ba4b66b80b9a
tomar2022 <tomarjrobinson@gmail.com>
2022-09-26 14:10:20 -0400

    first commit

---
+++ src/classProjectWhatsyourName/Howold.java
@@ -0,0 +1,29 @@
+package classProjectWhatsyourName;
+import java.util.*;
+
+
+public class Howold {
+
+	public static void main(String[] args) {
+		
+	Scanner scan = new Scanner(System.in);
+	System.out.println ("What's Your Name?");
+	String name = scan.next();
+	
+	System.out.println("What's Your Age");
+	int age = scan.nextInt();
+	
+	
+	System.out.println ("How long do you want to live?");
+	int live = scan.nextInt();
+	 
+	System.out.println("May The Force Be With YOU!" + name);
+	
+	
+	
+	
+			
+
+	}
+
+}
